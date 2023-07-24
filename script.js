e311a49043da462f4e388366f75aa2ed4631feae
Ousman Touray <otouray14@gmail.com>
2023-07-24 09:13:12 -0400

    feed page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let logInButton = document.querySelector('#log-in-button');
 
 logInButton.addEventListener('click', function(){
-    window.location.replace('http://127.0.0.1:5500/account.html')
+    window.location = ('http://127.0.0.1:5500/account.html');
 })
